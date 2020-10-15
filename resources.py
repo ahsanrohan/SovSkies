db83dc85fb52ac5302a0ca8688e88055b380caf9
pmarinelli18 <pmarinelli18@gmail.com>
2020-10-15 19:02:38 -0400

    added image

--- resources.py
+++ resources.py
@@ -37,6 +37,9 @@ level_map = pyglet.resource.image("level_background.png")
 level_map = pyglet.resource.image("level_background.png")
 center_image(level_map)
 
+sov_logo = pyglet.resource.image("SovLogo.png")
+center_image(sov_logo)
+
 # The engine flame should not be centered on the ship. Rather, it should be shown 
 # behind it. To achieve this effect, we just set the anchor point outside the
 # image bounds.
