367538f7bd619e04e186c5e8916f03ee79b8f63c
ahsanrohan <ahsanrohan@ufl.edu>
2020-10-18 20:53:45 -0400

    made setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SovSkies',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/ahsanrohan/SovSkies',
+    license='',
+    author='Ahsan',
+    author_email='ahsanrohan@ufl.edu',
+    description='Game Sovereign Skies'
+)
