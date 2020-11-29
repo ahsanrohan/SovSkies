d2a84a5a41ba6ca1996c452936df05278a51d72d
Ryan O'Keefe <44047602+rokeefe-UF@users.noreply.github.com>
2020-11-29 18:14:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -614,6 +614,7 @@ def level_menu():
     def on_mouse_press(x, y, button, modifiers):
         global mode
         global time
+        nonlocal levels_array
         if (windowWidth - x_button.width) < x < windowWidth and y > (
                 windowHeight - x_button.height):  # clicking X button
             mode = "menu"
@@ -623,23 +624,28 @@ def level_menu():
             mode = "game"
             time = 0
             start(0)
-        elif level_2[0].x - 100 < x < level_2[0].x + 100 and level_2[0].y - 100 < y < level_2[0].y + 100:
+        elif level_2[0].x - 100 < x < level_2[0].x + 100 and level_2[0].y - 100 < y < level_2[
+            0].y + 100 and levels_array[0][3] == 1:
             mode = "game"
             time = 0
             start(1)
-        elif level_3[0].x - 100 < x < level_3[0].x + 100 and level_3[0].y - 100 < y < level_3[0].y + 100:
+        elif level_3[0].x - 100 < x < level_3[0].x + 100 and level_3[0].y - 100 < y < level_3[
+            0].y + 100 and levels_array[1][3] == 1:
             mode = "game"
             time = 0
             start(2)
-        elif level_4[0].x - 100 < x < level_4[0].x + 100 and level_4[0].y - 100 < y < level_4[0].y + 100:
+        elif level_4[0].x - 100 < x < level_4[0].x + 100 and level_4[0].y - 100 < y < level_4[
+            0].y + 100 and levels_array[2][3] == 1:
             mode = "game"
             time = 0
             start(3)
-        elif level_5[0].x - 100 < x < level_5[0].x + 100 and level_5[0].y - 100 < y < level_5[0].y + 100:
+        elif level_5[0].x - 100 < x < level_5[0].x + 100 and level_5[0].y - 100 < y < level_5[
+            0].y + 100 and levels_array[3][3] == 1:
             mode = "game"
             time = 0
             start(4)
-        elif level_6[0].x - 100 < x < level_6[0].x + 100 and level_6[0].y - 100 < y < level_6[0].y + 100:
+        elif level_6[0].x - 100 < x < level_6[0].x + 100 and level_6[0].y - 100 < y < level_6[
+            0].y + 100 and levels_array[4][3] == 1:
             mode = "game"
             time = 0
             start(5)
